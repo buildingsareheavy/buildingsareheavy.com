6591b7b3862811b2025cafa66c75aada8747575e
Ethan Barrett <hello@buildingsareheavy.com>
2019-06-20 00:20:26 -0600

    first commit for Vuejs refactor

--- README.md
+++ README.md
@@ -7,3 +7,20 @@
 The objective was to combine my professional portfolio and information with my personal blog to give a more holistic view of who I am, as well as what I do.
 
 [Prototype in Figma](https://www.figma.com/proto/gWEANvhH1dZTGOPVmGP9Z60K/Buildings-Are-Heavy-dot-com?node-id=0%3A1&scaling=min-zoom)
+
+
+### Uses my Gridsome Blog Starter with Netlify CMS Starter - [https://github.com/buildingsareheavy/gridsome-blog-netlify-cms-starter](https://github.com/buildingsareheavy/gridsome-blog-netlify-cms-starter)
+
+
+## Based on the official Gridsome Starter Blog
+
+https://github.com/gridsome/gridsome-starter-blog.git
+
+### Deploy to Netlify
+
+Build Command: `gridsome build`
+Publish directory: `dist`
+
+### Demo Link
+
+https://gridsome-blog-netlify-cms-starter.netlify.com/
---
+++ src/components/Author.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="author">
+    <!-- <g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" /> -->
+
+    <h1 v-if="showTitle" class="author__site-title">{{ $static.metaData.siteName }}</h1>
+
+    <p
+      class="author__intro"
+    >A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.</p>
+  </div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+  props: ["showTitle"]
+};
+</script>
+
+<style lang="scss">
+.author {
+  margin: 0 auto;
+  max-width: 500px;
+  text-align: center;
+  padding: calc(var(--space) / 2) 0;
+
+  &__image {
+    border-radius: 100%;
+    width: 90px;
+    height: 90px;
+    margin-bottom: 1em;
+  }
+
+  &__intro {
+    opacity: 0.8;
+  }
+
+  &__site-title {
+    font-size: 1.5em;
+  }
+
+  &__links {
+    margin-top: -0.5em;
+    a {
+      margin: 0 0.5em;
+    }
+  }
+}
+</style>
