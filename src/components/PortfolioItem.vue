45b0b1ca9bd2c6d80fb05e8468fded62d3b2e270
Ethan Barrett <hello@buildingsareheavy.com>
2019-07-04 15:56:44 -0600

    Cannot get the portfolio item images to load

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Portfolio Item</h1>
+    <div class="portfolio__item" v-for="item in $static.portfolioItems.edges" :key="item.node.id">
+      <h2>{{ item.node.title }}</h2>
+      <p>{{ item.node.description_1 }}</p>
+
+      <p v-if="item.node.description_2">{{ item.node.description_2 }}</p>
+      <a :href="item.node.url_full">{{item.node.url_name}}</a>
+      <!-- This only works if you call the image somewhere else. I called "Put It On The Blog" below so that one should come up, but not the other ones... -->
+      <!-- You can try switching it to Portfolio.vue inside the pages directory and from <static-query> to <page-query> but I think it's still the same -->
+      <img :src="item.node.image" />
+    </div>
+    <img src="../assets/images/put-it-on-the-blog.jpg" />
+  </div>
+</template>
+
+
+<static-query>
+{
+  portfolioItems: allPortfolioItems(sortBy: "id", order: ASC) {
+    edges {
+      node {
+        id
+        title
+        description_1
+        description_2
+        url_full
+        url_name
+        image (width:200)
+      }
+    }
+  }
+}
+</static-query>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -3,39 +3,21 @@
     <div class="portfolio">
       <h1 class="page-title text-center space-bottom">Portfolio</h1>
 
-      <div class="portfolio__item" v-for="item in $page.portfolioItems.edges" :key="item.node.id">
-        <h2>{{ item.node.title }}</h2>
-        <p>{{ item.node.description_1 }}</p>
+      <PortfolioItem />
 
-        <p v-if="item.node.description_2">{{ item.node.description_2 }}</p>
-        <a :src="item.node.url_full">{{item.node.url_name}}</a>
-        <g-image :src="item.node.image" />
-      </div>
+      <!-- <img src="../assets/images/put-it-on-the-blog.jpg" /> -->
     </div>
   </Layout>
 </template>
 
-<page-query>
-{
-  portfolioItems: allPortfolioItems {
-    edges {
-      node {
-        id
-        title
-        description_1
-        description_2
-        url_full
-        url_name
-        image
-      }
-    }
-  }
-}
-</page-query>
+
 
 <script>
+import PortfolioItem from "../components/PortfolioItem";
 export default {
-  components: {},
+  components: {
+    PortfolioItem
+  },
   metaInfo: {
     title: "Portfolio"
   }
@@ -44,6 +26,9 @@ export default {
 
 <style lang="scss">
 .portfolio {
+  &__item {
+    border: 1px solid currentColor;
+  }
 }
 </style>
 
