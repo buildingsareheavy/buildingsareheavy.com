483f944011bf37e7a486a2f17d3bf68e120deb99
Ethan Barrett <hello@buildingsareheavy.com>
2019-07-04 16:02:26 -0600

    Cannot get the portfolio item images to load. Look in PortfolioItem.vue for comments

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -7,35 +7,48 @@
 
       <p v-if="item.node.description_2">{{ item.node.description_2 }}</p>
       <a :href="item.node.url_full">{{item.node.url_name}}</a>
-      <!-- This only works if you call the image somewhere else. I called "Put It On The Blog" below so that one should come up, but not the other ones... -->
-      <!-- You can try switching it to Portfolio.vue inside the pages directory and from <static-query> to <page-query> but I think it's still the same -->
+      <!-- Portfolio Images -->
       <img :src="item.node.image" />
     </div>
+    <!-- Independently called image -->
     <img src="../assets/images/put-it-on-the-blog.jpg" />
+
+    <!-- Calling portfolio.json from import, same problems -->
+    <!-- <div v-for="item in portfolio" :key="item.id">
+      <p>{{ item.title }}</p>
+      <g-image :src="item.image" alt />
+    </div>-->
   </div>
 </template>
 
 
-<static-query>
-{
-  portfolioItems: allPortfolioItems(sortBy: "id", order: ASC) {
-    edges {
-      node {
-        id
-        title
-        description_1
-        description_2
-        url_full
-        url_name
-        image (width:200)
-      }
-    }
-  }
-}
-</static-query>
 
 <script>
-export default {};
+// import portfolio from "../data/portfolio.json";
+export default {
+  // JSON data stored in /src/data/portfolio.json
+  // Config for calling JSON data is in gridsome.server.js
+  //
+  // Portfolio images only work if you call the image somewhere else outside of the v-for loop.
+  // I called "Put It On The Blog" below so that one should come up, but not the other ones...
+  // Below the independently called image, I also tried calling it without GraphQL, but it's the same problem. it won't GET the path of the images.
+  // I've tried a variety of different paths in the portfolio.json, but the current paths are the only ones that will show up (with another independent request that is).
+  // You can also try switching all of this to Portfolio.vue inside the pages directory and from <static-query> (for components) to <page-query> (for pages and templates) but I think it's still the same either way.
+  //
+  // REFERENCES: GRIDSOME LINKS
+  // Image usage via GraphQL - https://gridsome.org/docs/images/#usage-via-graphql
+  // Data - https://gridsome.org/docs/data/
+  //
+  // REFERENCES: GITHUB ISSUES
+  // g-images with dynamic paths not correctly generating full feature set - https://github.com/gridsome/gridsome/issues/292
+  // How to use g-image with graphQL? - https://github.com/gridsome/gridsome/issues/338
+  // Image handling  - https://github.com/gridsome/gridsome/issues/60
+  data() {
+    return {
+      portfolio
+    };
+  }
+};
 </script>
 
 <style>
