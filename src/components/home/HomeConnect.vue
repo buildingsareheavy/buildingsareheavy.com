ee4492c21ffc58d6ca7e1cafe2e3223882906a21
Ethan Barrett <chiquito@Ethans-MacBook-Pro.local>
2019-10-24 16:06:19 -0600

    fixed connect section

--- src/components/home/HomeConnect.vue
+++ src/components/home/HomeConnect.vue
@@ -1,10 +1,21 @@
 <template>
   <section class="home__connect">
     <h1>Connect</h1>
-    <div class="home__conntect-container">
-      <p v-for="link in links" :key="link.name">
-        <a :href="link.url">{{link.name}}</a>
-      </p>
+    <div class="home__connect-container">
+      <div>
+        <h2>Email</h2>
+        <a :href="links[0].url">{{ links[0].name }}</a>
+      </div>
+      <div>
+        <h2>Social</h2>
+        <a :href="links[1].url">{{ links[1].name }}</a>
+        <a :href="links[2].url">{{ links[2].name }}</a>
+      </div>
+      <div>
+        <h2>Code</h2>
+        <a :href="links[3].url">{{ links[3].name }}</a>
+        <a :href="links[4].url">{{ links[4].name }}</a>
+      </div>
     </div>
   </section>
 </template>
@@ -16,7 +27,7 @@ export default {
     return {
       links: [
         {
-          name: "Email",
+          name: "Hello@BuildingsAreHeavy.com",
           url: "mailto:hello@buildingsareheavy.com"
         },
 
@@ -46,7 +57,19 @@ export default {
 <style lang="scss">
 .home {
   &__connect {
-    &-title {
+    &-container {
+      @include bigger-than($tablet) {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 1rem;
+      }
+      div:not(:first-child) {
+        justify-self: center;
+      }
+      a {
+        padding-bottom: 10px;
+        display: block;
+      }
     }
   }
 }
