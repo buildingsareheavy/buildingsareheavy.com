210021139fe177e678955974532673597ad74995
Ethan Barrett <apartamento@Ethans-MacBook-Pro.local>
2019-11-14 18:24:24 -0700

    final check before live

--- src/components/about/AboutSkills.vue
+++ src/components/about/AboutSkills.vue
@@ -7,14 +7,25 @@
       </summary>
       <div class="about_skills-key">
         <h3>Front-End Web Development</h3>
-        <p>Specializing in an inclusive approach to web development, I create user-focused, responsive and performance-first websites and applications. I use semantic HTML and progressive enhancements in CSS and JavaScript to ensure the best experience and accessibility.</p>
+        <p>
+          Specializing in an inclusive approach to web development, I create
+          user-focused, responsive and performance-first websites and
+          applications. I use semantic HTML and progressive enhancements in CSS
+          and JavaScript to ensure the best experience and accessibility.
+        </p>
         <h4 class="bold">What does that mean to you?</h4>
-        <p
-          class="about__skills-break"
-        >That means I use best practices for the web to increase SEO (search engine optimiziation), faster page speeds, backwards compatiblitity, and overall better experience for your users.</p>
+        <p class="about__skills-break">
+          That means I use best practices for the web to increase SEO (search
+          engine optimiziation), faster page speeds, backwards compatiblitity,
+          and overall better experience for your users.
+        </p>
 
         <h3>Team Management, Leadership, and Consultancy</h3>
-        <p>Whether it is consulting businesses on best practices for their website, or training teams about technology and services, I have a proven track record of clear communication and effective management.</p>
+        <p>
+          Whether it is consulting businesses on best practices for their
+          website, or training teams about technology and services, I have a
+          proven track record of clear communication and effective management.
+        </p>
       </div>
     </details>
 
@@ -22,7 +33,11 @@
       <summary>
         <h2>List of Technical Skills</h2>
       </summary>
-      <div class="about__skills-tech-item" v-for="skill in skills" :key="skill.title">
+      <div
+        class="about__skills-tech-item"
+        v-for="skill in skills"
+        :key="skill.title"
+      >
         <h3>{{ skill.title }}</h3>
         <p>{{ skill.list }}</p>
       </div>
@@ -33,20 +48,19 @@
       </summary>
       <div class="about__skills-setup">
         <p class="subheading">
-          A more complete description of my development envirornment can be found at
-          <a
-            href="https://ando.netlify.com/"
-          >ando.netlify.com</a>
+          A more complete description of my development envirornment can be
+          found at
+          <a href="https://ando.netlify.com/">ando.netlify.com</a>
         </p>
         <p>
           I use
           <a href="https://hyper.is/">Hyper</a> as my terminal and
-          <a href="https://code.visualstudio.com/">Visual Studio Code</a> as my IDE. Hyper is configured with an
+          <a href="https://code.visualstudio.com/">Visual Studio Code</a> as my
+          IDE. Hyper is configured with an
           <a href="https://ohmyz.sh/">Oh-My-Zsh</a> setup and a
-          <a href="https://github.com/romkatv/powerlevel10k">Powerlevel10K</a> theme, and both Hyper and VS Code use a color syntax theme called
-          <a
-            href="https://taniarascia.github.io/new-moon/"
-          >New Moon</a>.
+          <a href="https://github.com/romkatv/powerlevel10k">Powerlevel10K</a>
+          theme, and both Hyper and VS Code use a color syntax theme called
+          <a href="https://taniarascia.github.io/new-moon/">New Moon</a>.
         </p>
 
         <img
@@ -57,29 +71,44 @@
 
         <p>
           I use
-          <a href="https://app.programmingfonts.org/#dejavu">DejaVu Sans Mono</a> for my command-line font, because it has great glyph support, and
-          <a
-            href="https://app.programmingfonts.org/#victor-mono"
-          >Victor Mono</a> for my text editor font. It supports ligatures and has a nice cursive italic font, similar to Dank Mono or Fira Code.
+          <a href="https://app.programmingfonts.org/#dejavu"
+            >DejaVu Sans Mono</a
+          >
+          for my command-line font, because it has great glyph support, and
+          <a href="https://app.programmingfonts.org/#victor-mono"
+            >Victor Mono</a
+          >
+          for my text editor font. It supports ligatures and has a nice cursive
+          italic font, similar to Dank Mono or Fira Code.
         </p>
         <div class="about__skills-setup--fonts">
-          <img src="../../assets/dejavu-sans-mono.jpg" alt="DejaVu Sans Mono font example" />
-          <img src="../../assets/victor-mono.jpg" alt="Victor Mono font example" />
+          <img
+            src="../../assets/dejavu-sans-mono.jpg"
+            alt="DejaVu Sans Mono font example"
+          />
+          <img
+            src="../../assets/victor-mono.jpg"
+            alt="Victor Mono font example"
+          />
         </div>
 
         <p>
           My favorite browser is
-          <a
-            href="https://www.mozilla.org/en-US/firefox/developer/"
-          >Firefox Developer Editon</a> because of it's excellent
+          <a href="https://www.mozilla.org/en-US/firefox/developer/"
+            >Firefox Developer Editon</a
+          >
+          because of it's excellent
           <a
             href="https://www.smashingmagazine.com/2019/10/guide-new-experimental-css-devtools-firefox/"
-          >new DevTools</a> and security. I also have incredible respect for
-          the
-          <a
-            href="https://www.youtube.com/mozilladeveloper"
-          >Mozilla Developer</a> team, and the people like
-          <a href="https://jensimmons.com/">Jen Simmons</a> that make Mozilla so amazing.
+            >new DevTools</a
+          >
+          and security. I also have incredible respect for the
+          <a href="https://www.youtube.com/mozilladeveloper"
+            >Mozilla Developer</a
+          >
+          team, and the people like
+          <a href="https://jensimmons.com/">Jen Simmons</a> that make Mozilla so
+          amazing.
         </p>
       </div>
     </details>
