2fd3d3e2f857e990f8aeec943e8d1173c940c43e
Ethan Barrett <apartamento@Ethans-MacBook-Pro.local>
2019-11-14 18:56:07 -0700

    put outline.js inside of @/data/

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import Vue from 'vue';
-import 'remove-focus-outline';
-import VueMq from 'vue-mq';
+import Vue from "vue";
+import "@/data/outline";
+import VueMq from "vue-mq";
 Vue.use(VueMq, {
   breakpoints: {
     sm: 480,
     md: 768,
-    lg: 992,
-  },
+    lg: 992
+  }
 });
 
-import DefaultLayout from '~/layouts/Default.vue';
+import DefaultLayout from "~/layouts/Default.vue";
 
 // Code styling is now in "_code.scss" (inside of index.scss)
 // import 'prismjs/themes/prism.css';
-import '@/assets/scss/index.scss';
+import "@/assets/scss/index.scss";
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout);
+  Vue.component("Layout", DefaultLayout);
 }
