176e4a8a8e9e937bf05f3ecf698d73d463b2a5cb
Ethan Barrett <apartamento@Ethans-MacBook-Pro.local>
2019-11-15 17:44:01 -0700

    added google analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,69 +1,69 @@
-const path = require('path');
+const path = require("path");
 
 function addStyleResource(rule) {
   rule
-    .use('style-resource')
-    .loader('style-resources-loader')
+    .use("style-resource")
+    .loader("style-resources-loader")
     .options({
-      patterns: [path.resolve(__dirname, './src/assets/scss/index.scss')],
+      patterns: [path.resolve(__dirname, "./src/assets/scss/index.scss")]
     });
 }
 
 module.exports = {
-  siteName: 'Buildings Are Heavy',
+  siteName: "Buildings Are Heavy",
   chainWebpack(config) {
     // Load variables for all vue-files
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
     types.forEach(type => {
-      addStyleResource(config.module.rule('scss').oneOf(type));
+      addStyleResource(config.module.rule("scss").oneOf(type));
     });
   },
   plugins: [
     {
-      use: '@gridsome/vue-remark',
+      use: "@gridsome/vue-remark",
       options: {
-        typeName: 'Photography',
-        baseDir: './photography',
-        pathPrefix: '/',
-        template: './src/templates/PhotographyPost.vue',
+        typeName: "Photography",
+        baseDir: "./photography",
+        pathPrefix: "/",
+        template: "./src/templates/PhotographyPost.vue",
         refs: {
           tags: {
-            typeName: 'PhotographyTag',
-            create: true,
-          },
-        },
-      },
+            typeName: "PhotographyTag",
+            create: true
+          }
+        }
+      }
     },
     {
-      use: '@gridsome/vue-remark',
+      use: "@gridsome/vue-remark",
       options: {
-        typeName: 'Writing',
-        baseDir: './writing',
-        pathPrefix: '/',
-        template: './src/templates/WritingPost.vue',
+        typeName: "Writing",
+        baseDir: "./writing",
+        pathPrefix: "/",
+        template: "./src/templates/WritingPost.vue",
         refs: {
           tags: {
-            typeName: 'WritingTag',
-            create: true,
-          },
-        },
-      },
+            typeName: "WritingTag",
+            create: true
+          }
+        }
+      }
     },
     {
-      use: '@gridsome/vue-remark',
+      use: "@gridsome/vue-remark",
       options: {
-        typeName: 'Portfolio',
-        baseDir: './portfolio',
-        pathPrefix: '/',
-      },
+        typeName: "Portfolio",
+        baseDir: "./portfolio",
+        pathPrefix: "/"
+      }
     },
     // configure Netlify CMS
     {
       use: `gridsome-plugin-netlify-cms`,
       options: {
         publicPath: `/admin`,
-        modulePath: `src/admin/index.js`,
-      },
+        modulePath: `src/admin/index.js`
+      }
     },
     // fix paths
     // {
@@ -73,13 +73,19 @@ module.exports = {
     //     coverField: 'image', // Global definition
     //   },
     // },
+    {
+      use: "@gridsome/plugin-google-analytics",
+      options: {
+        id: "UA-152620234-1"
+      }
+    }
   ],
   transformers: {
     //Add markdown support to all file-system sources
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-    },
-  },
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link"
+    }
+  }
 };
